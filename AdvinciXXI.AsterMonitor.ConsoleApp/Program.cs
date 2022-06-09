a1e7ef88f55d98a9175628f8b5d453262ee46f24
Andrew Paes <administrator@advincixxi.com.br>
2022-06-09 10:05:25 -0300

    First commit to generate other branches.

---
+++ AdvinciXXI.AsterMonitor.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdvinciXXI.AsterMonitor.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
